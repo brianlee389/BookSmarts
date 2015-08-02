3b660114bda5d033d252a30e404389c4c18157fc
Brian Lee <bl389@drexel.edu>
2015-08-02 18:17:56 -0400

    updated book query

--- app/controllers/Books.java
+++ app/controllers/Books.java
@@ -9,6 +9,8 @@ import play.db.ebean.Model;
 import play.mvc.*;
 import utils.DBUtils;
 import views.html.*;
+import java.util.HashMap;
+import java.util.Map;
 /*import play.mvc.Http.RequestBody;*/
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -23,11 +25,43 @@ public class Books extends Controller {
 
     public static Result index() {
     	List <Book> bookList = new Model.Finder(String.class, Book.class).all();
-        String names = "1";
-        for(Book b : bookList ) {
-        	names = names + b.name;
-        }
+
         return ok(books.render("", bookList));
+    }
+
+    public static Result search() {
+    	String sql = "select B.name, B.isbn, string_agg(A.name, ', '), P.name"
+			+ "from Books B, Authors A, Authored Ad, Publishers P, Published Pd"
+			+ "where B.isbn = Ad.book_isbn"
+			+ "and A.id = Ad.author_id"
+			+ "and B.isbn = Pd.book_isbn"
+			+ "and Pd.publisher_name = P.name"
+			+ "group by B.name, B.isbn, P.name";
+    	Connection conn = DB.getConnection();
+    	List<String> booklist = new List<String>();
+    	String json = "";
+    	try {
+    	 	Statement st = conn.createStatement();
+    	  	ResultSet rs = st.executeQuery(sql);
+    	  	ObjectMapper mapper = new ObjectMapper();
+			while (rs.next()) {
+				HashMap<String, String> b = new HashMap<String, String>();
+				b.put("book_name", rs.getString(1));
+				b.put("isbn", rs.getString(2));
+				b.put("authors", rs.getString(3));
+				b.put("publisher", rs.getString(4));
+            }
+            json = mapper.writeValueAsString(map);
+			booklist.add(json);
+    		rs.close();
+    		st.close();
+    		
+	    	DBUtils.closeDBConnection(conn);
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		}
+
+		return ok(toJson(bookList));
     }
 
 }
