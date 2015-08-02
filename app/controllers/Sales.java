7c8c4fb02c972e34089fd8c8501a212c08caddb1
Drew Banin <drew@8tracks.com>
2015-08-02 16:55:01 -0400

    code cleanup

--- app/controllers/Sales.java
+++ app/controllers/Sales.java
@@ -22,17 +22,12 @@ import java.util.List;
 import static play.libs.Json.toJson;
 
 public class Sales extends Controller {
-		//public static Result index() {
-		//		List <BookSale> book_sales = new Model.Finder(int.class, BookSale.class).all();
-		//		return ok(sales.render("drewbanin", book_sales));
-		//}
 
 		public static Result index() {
 			Connection conn = DB.getConnection();
 
 			ArrayList<HashMap<String, String>> res = new ArrayList<HashMap<String, String>>();
 			String logged_in_user = "drewbanin";
-			//book name, book isbn, vendor name, vendor link, price, sold at
 			String sql = "select bq.vendor_name, b.name, b.isbn, bs.price, bs.sold_at "
 				         + "from booksale bs join books b on bs.book_isbn = b.isbn "
 								 + "join bookquotes bq on bq.quote_id = bs.quote_id "
